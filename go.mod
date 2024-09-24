2b829f2f7469f3b40229d65edbb286b74f023386
Murad Mollaoglu <muradmollaoglu@gmail.com>
2024-09-24 09:27:10 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/muradmolla/imagetingx
+
+go 1.22.5
---
+++ image.go
@@ -0,0 +1,41 @@
+package imagetingx
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"os"
+)
+
+type Image struct {
+	input *os.File
+	img   *image.Image
+}
+
+func New(input string) (*Image, error) {
+	file, err := os.Open(input)
+	if err != nil {
+		msg := fmt.Sprintf("error while opening file. error: %s", err)
+		return nil, errors.New(msg)
+	}
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		msg := fmt.Sprintf("error while decoding jpeg. error: %s", err)
+		return nil, errors.New(msg)
+	}
+	return &Image{input: file, img: &img}, err
+}
+
+func (img *Image) save(output string) error {
+	img.input.Close()
+	fs, err := os.Create(output)
+	if err != nil {
+		msg := fmt.Sprintf("error while creating output. error: %s", err)
+		return errors.New(msg)
+	}
+
+	jpeg.Encode(fs, *img.img, nil)
+
+	return nil
+}
