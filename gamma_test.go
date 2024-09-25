fb0e56e6bfefac25a09f44f98668b6e36b76650c
Murad Mollaoglu <muradmollaoglu@gmail.com>
2024-09-25 14:59:40 +0300

    New test for gamma

--- gamma_test.go
+++ gamma_test.go
@@ -3,27 +3,48 @@ package imagetingx_test
 import (
 	"image"
 	"image/color"
+	"math/rand"
 	"testing"
 
 	"github.com/muradmolla/imagetingx"
 )
 
 func TestGamma(t *testing.T) {
-	img, err := imagetingx.New("test.jpg")
-	if err != nil {
-		t.Fatalf("cannot init image. error :%s", err)
+	// Testing both gamma functions match
+	img := createImage(increasingColor)
+	img.Gamma(2.0)
+	imgT := createImage(increasingColor)
+	imgT.BruteGamma(2.0)
+	r := compareImages(img, imgT)
+	if !r {
+		t.Errorf("Images does not match when gamma")
 	}
-	img.Gamma(0.5)
-	img.Save("test_gamma.jpg")
 }
 
-func createImage() imagetingx.ImgX {
+func increasingColor(x, y int) color.NRGBA {
+	return color.NRGBA{R: uint8(x % 255), G: uint8(y % 255), B: uint8((x + y) % 255), A: 255}
+}
+
+func randomColor(int, int) color.NRGBA {
+	rndFn := func() uint8 {
+		return uint8(rand.Float32() * 255)
+	}
+
+	return color.NRGBA{R: rndFn(), G: rndFn(), B: rndFn(), A: 255}
+}
+
+func createImage(imgFunc func(int, int) color.NRGBA) imagetingx.ImgX {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	for x := 0; x < 100; x++ {
 		for y := 0; y < 100; y++ {
-			img.Set(x, y, color.RGBA{0, 0, 0, 255})
+			img.Set(x, y, imgFunc(x, y))
 		}
 	}
 
-	return imagetingx.ImgX{img: img}
+	result, err := imagetingx.New(img)
+	if err != nil {
+		panic(err)
+	}
+
+	return *result
 }
